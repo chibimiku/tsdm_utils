291fc2ce41cceee9e5be74ed2ec9138c8787c72d
chibimiku <dreamcosine@gmail.com>
2016-11-02 16:57:30 +0800

    upate for nginx-in-windows utils

--- php/common_func.php
+++ php/common_func.php
@@ -1,5 +1,27 @@
 <?php 
 
+/* gets the data from a URL */
+function get_data($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch,CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0');
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+function getTagFromString($input, $startTag, $endTag, $detect = ''){
+	$rs = getTagsFromString($input, $startTag, $endTag, $detect);
+	if(!isset($rs[0])){
+		return false;
+	}else{
+		return $rs[0];
+	}
+}
+
 function getTagsFromString($input, $startTag, $endTag, $detect = ''){
 		
 		$result = array();
