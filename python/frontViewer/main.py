51d3780eaa067fdf3d1175f46895f4a6694b6cb8
chibimiku <dreamcosine@gmail.com>
2017-01-24 14:29:50 +0800

    fix

--- python/frontViewer/main.py
+++ python/frontViewer/main.py
@@ -21,12 +21,12 @@ class WindowsDriver:
         self.waitAfterBigOperation=5 #比较耗时的操作后等5秒
     
         mobileua = 'Mozilla/5.0 (Linux; U; Android 6.0.1; zh-tw; E5823 Build/32.2.A.0.305) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 Chrome/37.0.0.0 MQQBrowser/7.2 Mobile Safari/537.36';
-        chromeOptions = new ChromeOptions();
-        chromeOptions.AddArgument("--user-agent=" + mobileua);
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--user-agent=" + mobileua)
     
         chromedriverPath='lib/chromedriver.exe'
         os.environ["webdriver.chrome.driver"] = chromedriverPath
-        self.driver = webdriver.Chrome(chromedriverPath, ChromeOptions)
+        self.driver = webdriver.Chrome(chromedriverPath, chrome_options=chrome_options)
         self.driver.set_page_load_timeout(self.loadpageTimeout)
         self.plog ("Driver started...")
     
