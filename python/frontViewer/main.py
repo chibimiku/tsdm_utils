1b89080d1d29ebd2b1cca2f3bd0e930c1c6ae270
chibimiku <dreamcosine@gmail.com>
2017-01-24 14:27:58 +0800

    update custume ua

--- python/frontViewer/main.py
+++ python/frontViewer/main.py
@@ -20,9 +20,13 @@ class WindowsDriver:
         self.waitAfterOperation=1.5 #操作后等1秒
         self.waitAfterBigOperation=5 #比较耗时的操作后等5秒
     
+        mobileua = 'Mozilla/5.0 (Linux; U; Android 6.0.1; zh-tw; E5823 Build/32.2.A.0.305) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 Chrome/37.0.0.0 MQQBrowser/7.2 Mobile Safari/537.36';
+        chromeOptions = new ChromeOptions();
+        chromeOptions.AddArgument("--user-agent=" + mobileua);
+    
         chromedriverPath='lib/chromedriver.exe'
         os.environ["webdriver.chrome.driver"] = chromedriverPath
-        self.driver = webdriver.Chrome(chromedriverPath)
+        self.driver = webdriver.Chrome(chromedriverPath, ChromeOptions)
         self.driver.set_page_load_timeout(self.loadpageTimeout)
         self.plog ("Driver started...")
     
